38fa28adcd3447b6c684a026e19cc2d51ab0a0ac
muhammed <sabith4400@gmail.com>
2025-02-04 14:32:02 +0530

    edit_profile completed and color_constant added

---
+++ lib/Common/color_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+    static Color primaryColor = const Color(0xff4B164C);
+    static Color whiteColor = const Color(0xffD9D9D9);
+    static Color roseColor = const Color(0xFFDD88CF);
+    static Color lightroseColor = const Color(0xFFC4C4C4);
+    static Color blurColor = const Color(0xB39d5a75);
+}
---
+++ lib/profile/screens/edit_profile.dart
@@ -0,0 +1,274 @@
+import 'package:flutter/material.dart';
+import 'package:buddy_pair/Common/color_constants.dart';
+import 'package:buddy_pair/Common/Text.dart';
+
+class EditProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff4B164C),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: AppBar(
+          backgroundColor: Color(0xff4B164C),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(100)),
+              child: IconButton(
+                onPressed: () {},
+                icon: Padding(
+                  padding: const EdgeInsets.only(left: 4),
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: Color(0xffD9D9D9),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          flexibleSpace: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: Text(
+                    'Edit My Profile',
+                    style: TextStyles().heading1(color: Color(0xffD9D9D9)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40),
+              topRight: Radius.circular(40),
+            ),
+          ),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Image.asset('assets/images/Rectangle 1131.png'),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/Ellipse 310.png'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Nazrul Islam',
+                        style: TextStyles().bodyText(color: Colors.black),
+                      ),
+                      Text(
+                        'Never give up 💪',
+                        style: TextStyles().smallText(color: Color(0xff797C7B)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20),
+                ],
+              ),
+              Text(
+                'All your account information can be accessed and edited here but your mail will still remain un-edited.',
+                style: TextStyles().smallText(color: Colors.black),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Name',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Username',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Phone Number',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Bio',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Images',
+                    style: TextStyles().smallText(color: Colors.black),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/Shape.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/Shape.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/Shape.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.add),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Reels',
+                    style: TextStyles().smallText(color: Colors.black),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/Shape.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Change Password',
+                    style: TextStyles().smallText(color: Colors.black),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: 200,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Color(0xff4B164C),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Update',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
